9baa081b98ed8bdd2b2edbb6083083b73a4c970e
jonatan <jonatan.haaralaorosco@gmail.com>
2022-03-15 01:07:16 -0300

    test new branch

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -17,6 +17,9 @@ export default function Home () {
                 </div>
             </div>
             
+            <div>
+                <h1>hola hola</h1>
+            </div>
         </div>
     );
 }
